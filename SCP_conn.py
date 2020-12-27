d52b12a441941c7dfb17d9728baf92c26ee26119
bodnar41 <36450829+bodnar41@users.noreply.github.com>
2020-12-27 17:58:06 +0100

    updated

--- SCP_conn.py
+++ SCP_conn.py
@@ -1,12 +1,15 @@
 import paramiko, socket, sys
 
-def scp_conn(ip = None, user = None, pwd = None):
+def scp_conn(ip = None, user = None, pwd = None, action = None):
     if ip is None:
         ip = input('[*]Enter Server IP Address :')
     if user is None:
         user = input('[*]Enter Username :')
     if pwd is None:
         pwd = input('[*]Enter Password :')
+    if action is None:
+        action = input('Put or get?')
+
     try:
         print("Creating SSH Client..")
         ssh_client = paramiko.SSHClient()
@@ -14,19 +17,21 @@ def scp_conn(ip = None, user = None, pwd = None):
         ssh_client.connect(hostname=ip, username=user, password=pwd)
         sftp_client = ssh_client.open_sftp()
         print("File transferring")
-        sftp_client.put("E:/cpu_info_to_json.py", "C:/Users/kliens02/Desktop/info.py")
+        if action.lower() == "put":
+            sftp_client.put("C:/Users/kliens01/cpu_info_if.py", "C:/Users/kliens02/info.py")
+            sftp_client.put("C:/Users/kliens01/teszt.txt", "C:/Users/kliens02/teszt.txt")
+        if action.lower() == "get":
+            sftp_client.get("C:/Users/kliens02/system_info.json", "C:/Users/kliens01/system_info.json")
         sftp_client.close()
         ssh_client.close()
     except paramiko.ssh_exception.AuthenticationException as e:
         print("Username: " + user + "\t or password invalid.")
-        scp_conn(ip, None, None)
+        scp_conn(ip, None, None, action)
     except FileNotFoundError:
-        print("Helytelen fájleléri útvonal")
+        print("Incorrect path.")
     except socket.gaierror:
         print(ip + " invalid.")
-        scp_conn(None, user, pwd)
+        scp_conn(None, user, pwd, action)
     except:
         print("Unexpected error: ", sys.exc_info())
 
-
-scp_conn()

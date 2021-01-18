6bc69e07ed99bd9e0cae170ba120b661ebe716a6
bodnar41 <b.maark97@gmail.com>
2021-01-18 11:44:18 +0100

    Updated scp_conn

--- SCP_conn.py
+++ SCP_conn.py
@@ -1,14 +1,16 @@
 import paramiko, socket, sys
 
-def scp_conn(ip = None, user = None, pwd = None, action = None):
+def scp_conn(ip = None, user = None, pwd = None, action = None, filename = None):
     if ip is None:
-        ip = input('[*]Enter Server IP Address :')
+        ip = input("Enter Server IP Address :")
     if user is None:
-        user = input('[*]Enter Username :')
+        user = input("Enter Username :")
     if pwd is None:
-        pwd = input('[*]Enter Password :')
+        pwd = input("Enter Password :")
     if action is None:
-        action = input('Put or get?')
+        action = input("Put or get:")
+    if filename is None:
+        filename = input("Enter the filename with extension:")
 
     try:
         print("Creating SSH Client..")
@@ -18,20 +20,22 @@ def scp_conn(ip = None, user = None, pwd = None, action = None):
         sftp_client = ssh_client.open_sftp()
         print("File transferring")
         if action.lower() == "put":
-            sftp_client.put("C:/Users/kliens01/cpu_info_if.py", "C:/Users/kliens02/info.py")
-            sftp_client.put("C:/Users/kliens01/teszt.txt", "C:/Users/kliens02/teszt.txt")
-        if action.lower() == "get":
-            sftp_client.get("C:/Users/kliens02/system_info.json", "C:/Users/kliens01/system_info.json")
+            sftp_client.put(f"C:/Users/kliens01/{filename}", f"C:/Users/{user}/{filename}")
+        elif action.lower() == "get":
+            sftp_client.get(f"C:/Users/{user}/{filename}", f"C:/Users/kliens01/{filename}")
+        else:
+            print("Enter valid action (put or get) :")
+            scp_conn(ip, user, pwd, None, filename)
         sftp_client.close()
         ssh_client.close()
     except paramiko.ssh_exception.AuthenticationException as e:
         print("Username: " + user + "\t or password invalid.")
-        scp_conn(ip, None, None, action)
+        scp_conn(ip, None, None, action, filename)
     except FileNotFoundError:
         print("Incorrect path.")
+        scp_conn(ip, user, pwd, action, None)
     except socket.gaierror:
         print(ip + " invalid.")
-        scp_conn(None, user, pwd, action)
+        scp_conn(None, user, pwd, action, filename)
     except:
-        print("Unexpected error: ", sys.exc_info())
-
+         print("Unexpected error: ", sys.exc_info())

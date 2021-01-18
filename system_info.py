e22082798f47d3a1c4164774c15e110379a6d7e4
bodnar41 <b.maark97@gmail.com>
2021-01-18 11:49:35 +0100

    Updated sysinfo modul

--- system_info.py
+++ system_info.py
@@ -3,10 +3,23 @@ from datetime import datetime
 import json
 import paramiko
 
+#Client name
+uname = platform.uname()
+clientname= uname.node.strip('-PC')
 
-f = open("C:/Users/kliens02/teszt.txt", "r")
-choice = f.read()
-print(choice)
+received_data = []
+with open(f"C:/Users/{clientname}/teszt.txt", 'r') as filehandle:
+    for line in filehandle:
+        # remove linebreak which is the last character of the string
+        currentPlace = line[:-1]
+
+        # add item to the list
+        received_data.append(currentPlace)
+
+# f = open("C:/Users/kliens02/teszt.txt", "r")
+choice = received_data[0]
+user = received_data[1]
+# print(choice)
 
 if choice == "1":
     uname = platform.uname()
@@ -14,15 +27,13 @@ if choice == "1":
         {"System": uname.system, "Node Name": uname.node, "Release": uname.release, "Version": uname.version,
          "Machine": uname.machine, "Processor": uname.processor}]}
     sysJSON = json.dumps(sysinfo, indent=4, sort_keys=True)
-    with open("C:/Users/kliens02/system_info.json", "w") as file:
-        json.dump(sysinfo, file, indent=4)
+
 
 if choice == "2":
     cpuinfo = {"CPU info": [
         {"Physical cores": psutil.cpu_count(logical=False), "Total cores": psutil.cpu_count(logical=True)}]}
     cpuJSON = json.dumps(cpuinfo, indent=4, sort_keys=True)
-    with open("C:/Users/kliens02/system_info.json", "w") as file:
-        json.dump(cpuinfo, file, indent=4)
+
 
 if choice == "3":
     cpufreq = psutil.cpu_freq()
@@ -30,8 +41,7 @@ if choice == "3":
         {"Max Frequency": str(cpufreq.max) + " Mhz", "Min Frequency": str(cpufreq.min) + " Mhz",
          "Current Frequency": str(cpufreq.current) + " Mhz"}]}
     freqJSON = json.dumps(freqinfo, indent=4, sort_keys=True)
-    with open("C:/Users/kliens02/system_info.json", "w") as file:
-        json.dump(freqinfo, file, indent=4)
+
 
 if choice == "4":
     core = []
@@ -46,6 +56,8 @@ if choice == "4":
         coreinfo["Core info"].append({'Core' + str(i): str(core[i]) + "%"})
     coreinfo["Core info"].append({'Full usage': str(data3) + "%"})
     coreJSON = json.dumps(coreinfo, indent=4, sort_keys=True)
-    with open("C:/Users/kliens02/system_info.json", "w") as file:
-        json.dump(coreinfo, file, indent=4)
+
+
+with open(f"C:/Users/{clientname}/system_info.json", "w") as file:
+    json.dump(sysinfo, file, indent=4)
 

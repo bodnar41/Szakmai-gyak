783b363520b7adc8a65385d315f101c5d8457721
bodnar41 <36450829+bodnar41@users.noreply.github.com>
2021-02-04 22:37:55 +0100

    Delete src directory

--- src/devicemgmt/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Device
-
-
-admin.site.register(Device)
--- src/devicemgmt/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DevicemgmtConfig(AppConfig):
-    name = 'devicemgmt'
